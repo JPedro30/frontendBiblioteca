5c145841cd5e014a177943dcb7c1ffa30e4db4a5
JPedro-ESP <jpgonzalezhens@gmail.com>
2026-06-05 15:32:53 +0200

    cambio divs laterales

--- src/App.jsx
+++ src/App.jsx
@@ -45,8 +45,8 @@ function App() {
     <div className="min-h-screen bg-[#F4EBE1] flex flex-col relative text-amber-950">
       
       {/* 2. LAS ENREDADERAS (Fijas a los lados, no estorban al hacer clic gracias a pointer-events-none) */}
-      <div className="fixed top-0 left-0 w-24 md:w-40 h-full bg-[url('/enredadera.png')] bg-repeat-y opacity-80 pointer-events-none z-0"></div>
-      <div className="fixed top-0 right-0 w-24 md:w-40 h-full bg-[url('/enredadera.png')] bg-repeat-y opacity-80 pointer-events-none z-0 scale-x-[-1]"></div>
+      <div className="fixed top-0 left-0 w-24 md:w-60 h-full bg-[url('/enredadera.png')] bg-repeat-y opacity-80 pointer-events-none z-0"></div>
+      <div className="fixed top-0 right-0 w-24 md:w-60 h-full bg-[url('/enredadera.png')] bg-repeat-y opacity-80 pointer-events-none z-0 scale-x-[-1]"></div>
 
       {/* 3. EL CONTENIDO PRINCIPAL (Con z-10 para estar por encima de las hojas) */}
       <div className="relative z-10 flex flex-col min-h-screen">
