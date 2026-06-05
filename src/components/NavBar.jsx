cbf073b8c3f103190733854923ce945a078fd064
JPedro-ESP <jpgonzalezhens@gmail.com>
2026-06-05 14:04:11 +0200

    canbios visuales ajustar tamaños

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,17 +6,17 @@ export default function Navbar({ setVista }) {
 
       {/* Grupo de Logo y Texto */}
       <div
-        className="flex items-center gap-2 md:gap-3 cursor-pointer transition-transform duration-200 hover:scale-105"
+        className="flex items-center gap-6 md:gap-6 cursor-pointer transition-transform duration-200 hover:scale-105"
         onClick={() => setVista('estanteria')}
       >
         <img
           src="/logoLibrary.png"
           alt="Logo Biblioteca"
-          className="w-8 h-8 md:w-14 md:h-14 object-contain"
+          className="w-12 h-12 mb-4 md:w-22 md:h-22 object-contain"
         />
         {/* Texto más pequeño en móvil (text-xl) y normal en PC (md:text-2xl) */}
         <div className="text-xl md:text-4xl font-black tracking-widest bg-purple-600 bg-clip-text text-transparent text-center">
-          Susana's Library
+          SUSANA'S LIBRARY
         </div>
       </div>
 
@@ -26,12 +26,12 @@ export default function Navbar({ setVista }) {
         {/* Enlace 1: Buscador */}
         <li 
           onClick={() => setVista('busqueda')} 
-          className="flex items-center gap-2 hover:text-purple-400 cursor-pointer transition-all duration-200"
+          className="flex flex-col items-center gap-0 hover:text-purple-400 cursor-pointer transition-all duration-200"
         >
           <img
             src="/buscarLibro.png"
             alt="Buscar"
-            className="w-4 h-4 md:w-7 md:h-7 object-contain"
+            className="w-8 h-8 md:w-14 md:h-14 object-contain"
           />
           <span>Buscador</span>
         </li>
@@ -39,12 +39,12 @@ export default function Navbar({ setVista }) {
         {/* Enlace 2: Agregar Libro */}
         <li 
           onClick={() => setVista('formulario')} 
-          className="flex items-center gap-2 cursor-pointer transition-all duration-300 text-purple-400 hover:text-fuchsia-300 font-semibold"
+          className="flex flex-col items-center gap-2 cursor-pointer transition-all duration-300 text-purple-400 hover:text-fuchsia-300 font-semibold"
         >
           <img
             src="/agregarLibro.png"
             alt="Agregar"
-            className="w-4 h-4 md:w-7 md:h-7 object-contain"
+            className="w-6 h-6 md:w-12 md:h-12 object-contain"
           />
           <span>Agregar Libro</span>
         </li>
